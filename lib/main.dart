924cf8fb8b916dd4bb0b268188d9ff13f533146b
cinna03 <c.mwiza1@alustudent.com>
2025-11-26 15:19:47 +0200

    config

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialize Firebase with platform-specific options
-  if (kIsWeb) {
-    // Web initialization
-    await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "AIzaSyDbn-Uij-5QkrsUl93NT94oymWslr68AnI",
-        authDomain: "hermony-1178f.firebaseapp.com",
-        projectId: "hermony-1178f",
-        storageBucket: "hermony-1178f.appspot.com",
-        messagingSenderId: "980797910254",
-        appId: "1:980797910254:web:04ea68e83cd17a5b3a17ff",
-      ),
-    );
-  } else {
-    // Mobile initialization
-    await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "AIzaSyDbn-Uij-5QkrsUl93NT94oymWslr68AnI",
-        appId: "1:980797910254:android:04ea68e83cd17a5b3a17ff",
-        messagingSenderId: "980797910254",
-        projectId: "hermony-1178f",
-        storageBucket: "hermony-1178f.appspot.com",
-      ),
-    );
-  }
+  // Load environment variables
+  await dotenv.load(fileName: ".env");
+
+  // Initialize Firebase with config from .env
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: dotenv.env['FIREBASE_API_KEY']!,
+      authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN'],
+      projectId: dotenv.env['FIREBASE_PROJECT_ID'],
+      storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'],
+      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'],
+      appId: dotenv.env['FIREBASE_APP_ID'],
+    ),
+  );
 
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hermony App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Hermony Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
 }
